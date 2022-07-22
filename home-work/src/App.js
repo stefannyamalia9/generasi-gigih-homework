9657e465fdefd17c04c5dd805fe57ba5429d83f0
stefannyamalia9 <stefannyamalia.work@gmail.com>
2022-07-22 13:50:44 +0700

    add m2 s2 convert elements into components

--- home-work/src/App.js
+++ home-work/src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
-import data from './data/dummy-data';
+import Homepage from './pages/home';
+
 function App() {
   return (
     <div className="App">
-      <h1>Spotify Playlist</h1>
-      <section>
-        <div className='container'>
-          <img src={data.album.images[0].url} alt=''/>
-          <p>Title : </p>
-          <p className='value'>{data.album.name}</p>
-          <p>Album : </p>
-          <p className='value'>{data.name}</p>
-          <p>Artist : </p>
-          <p className='value'>{data.artists[0].name}</p>
-          <button>Select</button>
-        </div>
-      </section>
+      <Homepage />
     </div>
   );
 }
